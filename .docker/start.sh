4dd604e8ee0e67a296abad1ce014c8b566198404
marcos.cardoso <marcos.cardoso@zamp.com.br>
2025-08-04 15:23:06 -0300

    Arquivos para Docker

--- .docker/start.sh
+++ .docker/start.sh
@@ -3,5 +3,11 @@
 # Inicia o PHP-FPM em primeiro plano
 php-fpm -F &
 
+# Aguarda um momento para o socket ser criado
+sleep 1
+
+# Ajusta as permissões do socket
+chmod 777 /var/www/html/run/php-fpm.sock
+
 # Inicia o Nginx em primeiro plano
 nginx -g "daemon off;"
