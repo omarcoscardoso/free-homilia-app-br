48955e904de12eb65f4a93a795d02c53db92cc00
marcos.cardoso <marcos.cardoso@zamp.com.br>
2026-06-29 11:50:45 -0300

    chore(ci): modificação para CI/CD

--- .docker/start.sh
+++ .docker/start.sh
@@ -5,6 +5,9 @@ php /var/www/html/artisan config:cache
 php /var/www/html/artisan route:cache
 php /var/www/html/artisan view:cache
 
+# Ajusta permissões dos diretórios de cache e logs gerados para o usuário www-data (PHP-FPM)
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
 # Inicia o PHP-FPM em segundo plano
 php-fpm -D
 
